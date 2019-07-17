84d093b004df692d0492800c05417551102bf1b8
罗晟 <wb-ls553774@alibaba-inc.com>
2019-07-17 17:14:07 +0800

    update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, OnInit, Inject } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+const RESULT_KEY = makeStateKey<string>('result');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-server';
+export class AppComponent implements OnInit {
+  public title: string = 'angular-server';
+  public result: any;
+  public isServer: boolean;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private tsState: TransferState
+  ) {}
+
+  ngOnInit(): void {
+    this.isServer = isPlatformServer(this.platformId);
+
+    if (this.isServer) {
+      // on the server
+      this.tsState.set(RESULT_KEY, 'I am created on the server');
+    } else {
+      if (this.tsState.hasKey(RESULT_KEY)) {
+        this.result = this.tsState.get(RESULT_KEY, '');
+        console.log(this.result);
+      } else {
+        this.result = 'I am created on the browser';
+        console.log(this.result);
+      }
+    }
+  }
 }
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import {
+  ServerModule,
+  ServerTransferStateModule
+} from '@angular/platform-server';
+// import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+  imports: [
+    AppModule,
+    ServerModule,
+    // ModuleMapLoaderModule,
+    ServerTransferStateModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppServerModule {}
